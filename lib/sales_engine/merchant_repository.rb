a2d7d12fafc56c3782386cf7d826e552ec2aef06
Ben Lewis <bennlewis@gmail.com>
2013-09-23 15:38:23 -0600

    added transaction_repository

--- lib/sales_engine/merchant_repository.rb
+++ lib/sales_engine/merchant_repository.rb
@@ -12,3 +12,18 @@ class MerchantRepository
   end
 
 end
+
+#loads up the merchants from the csv
+#each merchant in the repo responds to the merhcant class methods
+
+#build the single objects that take has of data
+# can responds to mehtods
+# give it fake data to make sure it responds
+
+# then build the repos
+# then add their methods
+#create fixtrues with three lines of data
+# fixtures should be csv files
+
+#then create the sales engine itself
+
---
+++ lib/sales_engine/transaction_repository.rb
@@ -0,0 +1,13 @@
+class TransactionRepository
+  def all
+  end
+
+  def random
+  end
+
+  def find_by_X(match)
+  end
+
+  def find_all_by_X(match)
+  end
+end
