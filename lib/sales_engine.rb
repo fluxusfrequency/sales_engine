b232391044fdc78a149cdf7c315ec240f9741814
Ben Lewis <bennlewis@gmail.com>
2013-09-23 14:56:16 -0600

    required classes in main file, built empty tests

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,6 +1,13 @@
 require 'csv'
+require_relative './sales_engine/customer'
+require_relative './sales_engine/customer_repository'
+require_relative './sales_engine/invoice'
+require_relative './sales_engine/invoice_repository'
+require_relative './sales_engine/invoice_item'
+require_relative './sales_engine/invoice_item_repository'
 require_relative './sales_engine/merchant'
 require_relative './sales_engine/merchant_repository'
+require_relative './sales_engine/transaction'
 
 class SalesEngine
 
