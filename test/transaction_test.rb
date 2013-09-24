f14fb48bda0377e34716dd2945263324f084f5b8
Ben Lewis <bennlewis@gmail.com>
2013-09-23 21:45:59 -0600

    updated transaction to match merchant and item hash args and tests pattern

--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -7,10 +7,21 @@ class TransactionTest < Minitest::Test
   attr_accessor :transaction
 
   def setup
-    @transaction = Transaction.new(1, "Schroeder-Jerde", "2012-03-27 14:53:59 UTC", "2012-03-27 14:53:59 UTC")
+    data = {:id => 1, :invoice_id => 1, :credit_card_number => 4654405418249632, :credit_card_expiration_date => "", :result => "success", :created_at => "2012-03-27 14:54:09 UTC", :updated_at => "2012-03-27 14:54:09 UTC"}
+    @transaction = Transaction.new(data)
   end
 
-  def test
-    true
+  def test_it_returns_a_transaction_object
+    assert_equal Transaction, transaction.class
+  end
+
+  def test_it_sets_up_attrs
+    assert_equal 1, transaction.id
+    assert_equal 1, transaction.invoice_id
+    assert_equal 4654405418249632, transaction.credit_card_number
+    assert_equal "", transaction.credit_card_expiration_date
+    assert_equal "success", transaction.result
+    assert_equal "2012-03-27 14:54:09 UTC", transaction.created_at
+    assert_equal "2012-03-27 14:54:09 UTC", transaction.updated_at
   end
 end
