3ba63fba9f81f759037f5ed0994d701f67a9ea2b
Ben Lewis <bennlewis@gmail.com>
2013-09-23 18:23:01 -0600

    built basic item with init taking params and setup the test

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,7 +1,18 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/sales_engine/item.rb'
 
 class ItemTest < Minitest::Test
+
+  attr_accessor :item
+
+  def setup
+    @item = Item.new("Item Qui Esse", "Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro.", 75107, 1, "2012-03-27", "14:53:59 UTC", "2012-03-27 14:53:59 UTC")
+  end
+
+  def test
+    true
+  end
 
 end
