0d5a402fc4e78d777153684292f819851a0ad390
Louisa Barrett <louisabarrett@gmail.com>
2013-09-24 10:02:26 -0600

    updates to basic objects tests

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -8,15 +8,30 @@ class CustomerTest < Minitest::Test
   attr_accessor :customer
 
   def setup
-    @customer = Customer.new(1,2,3,4,5)
+    data = {:id => 1,
+            :first_name => "Joey"
+            :last_name => "Ondricka"
+            :created_at => "2012-03-27 14:54:09 UTC"
+            :updated_at => "2012-03-27 14:54:09 UTC"
+          }
+
+    @customer = Customer.new(data)
   end
 
-  # def test_initializes_from_a_hash_of_data
-  #   customer.
-  #
+  def test_it_returns_an_item_object
+    assert_equal Customer, customer.class
+  end
 
-  def test_it_loads_a_file
-    assert_equal CSV, customer.load("./data/customers.csv").class
+  def test_it_sets_up_attrs
+    assert_equal 1, customer.id
+    assert_equal "Joey", customer.first_name
+    assert_equal "Ondricka", customer.last_name
+    assert_equal "2012-03-27 14:54:09 UTC", customer.created_at
+    assert_equal "2012-03-27 14:54:09 UTC", customer.updated_at
+  end
+
+  def test_it_has_an_invoice
+    assert true, customer.invoice
   end
 
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -1,7 +1,55 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative './lib/sales_engine/invoice_test.rb'
 
 class InvoiceTest < Minitest::Test
 
+  attr_accessor :invoice
+
+  def setup
+    data = {:id => 1
+            :customer_id => 1
+            :merchant_id => 26
+            :status => "shipped"
+            :created_at => "2012-03-25 09:54:09 UTC"
+            :updated_at => "2012-03-25 09:54:09 UTC"
+            }
+   @invoice = Invoice.new(data)
+  end
+
+  def test_it_returns_an_item_object
+    assert_equal Invoice, invoice.class
+  end
+
+  def test_it_sets_up_attrs
+    assert_equal 1, invoice.id
+    assert_equal 1, invoice.customer_id
+    assert_equal 26, invoice.merchant_id
+    assert_equal "shipped", invoice.status
+    assert_equal "2012-03-25 09:54:09 UTC", invoice.created_at
+    assert_equal "2012-03-25 09:54:09 UTC", invoice.updated_at
+  end
+
+  def test_it_has_a_transactions_method
+    assert true, invoice.transaction
+  end
+
+  def test_it_has_an_invoice_items_method
+    assert true, invoice.invoice_items
+  end
+
+  def test_it_has_an_items_method
+    assert true, invoice.items
+  end
+
+  def test_it_has_a_customer_method
+    assert true, invoice.customer
+  end
+
+  def test_it_has_a_merchant_method
+    assert true, invoice.merchant
+  end
+
 end
+
