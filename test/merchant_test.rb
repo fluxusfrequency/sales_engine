c7de27025869c9829d8012adcbc4073673bf4c8e
Ben Lewis <bennlewis@gmail.com>
2013-09-23 18:27:43 -0600

    created merchant class with init taking params and setup test

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,7 +1,18 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/sales_engine/merchant.rb'
 
 class MerchantTest < Minitest::Test
+
+  attr_accessor :merchant
+
+  def setup
+    @merchant = Merchant.new(1, "Schroeder-Jerde", "2012-03-27 14:53:59 UTC", "2012-03-27 14:53:59 UTC")
+  end
+
+  def test
+    true
+  end
 
 end
