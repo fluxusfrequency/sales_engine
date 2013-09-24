10a0b5c9e66b20f8394d22a73263b0acd5f14eca
Ben Lewis <bennlewis@gmail.com>
2013-09-23 18:35:20 -0600

    created transaction class and test

--- lib/sales_engine/transaction.rb
+++ lib/sales_engine/transaction.rb
@@ -1,6 +1,19 @@
 class Transaction
+  attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at
+
+  def initialize(id, invoice_id, credit_card_number, credit_card_expiration_date, result, created_at, updated_at)
+    @id = id
+    @name = name
+    @credit_card_number = credit_card_number
+    @credit_card_expiration_date = credit_card_expiration_date
+    @result = result
+    @created_at = created_at
+    @updated_at = updated_at
+  end
+
   def invoice
   end
+
 end
 
 # references only the invoice
--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -1,7 +1,16 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require_relative '../lib/sales_engine/transaction.rb'
 
 class TransactionTest < Minitest::Test
+  attr_accessor :transaction
 
+  def setup
+    @transaction = Transaction.new(1, "Schroeder-Jerde", "2012-03-27 14:53:59 UTC", "2012-03-27 14:53:59 UTC")
+  end
+
+  def test
+    true
+  end
 end
