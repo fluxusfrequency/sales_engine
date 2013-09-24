0708d0b1dff259521ea804f3ff8ff5117544bb67
Ben Lewis <bennlewis@gmail.com>
2013-09-23 18:39:32 -0600

    test returns an item obj

--- test/item_test.rb
+++ test/item_test.rb
@@ -11,8 +11,8 @@ class ItemTest < Minitest::Test
     @item = Item.new("Item Qui Esse", "Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro.", 75107, 1, "2012-03-27", "14:53:59 UTC", "2012-03-27 14:53:59 UTC")
   end
 
-  def test
-    true
+  def test_it_returns_an_item_object
+    assert_equal Item, item.class
   end
 
 end
